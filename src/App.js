cd86b8bd91a7e08e3d9bd3937751c8801515c9bf
namatoj <namatoj@gmail.com>
2022-05-26 15:39:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import * as utils from './utils.js'
+import { useState} from 'react';
 
 function App() {
+
+  const [terms, ] = useState(utils.getTerms());
+
+
+  const dayScale = 0.5;
+  const marginPixels = 2;
+  const orderScale = 25
+
+  const renderTerms = () => {
+    return terms.map((term, index) => {
+      return (
+        <>
+        <div className={`term ${term.party}`} key={index} style={
+          {
+            top:`${orderScale*(Number(term.ordningsnummer)-1)}px`,
+            left:`${dayScale * utils.daysBetween("1991-09-30",term.from)}px`,
+            width: `${dayScale * utils.daysBetween(term.from, term.to) - marginPixels}px`,
+            height: `${orderScale - 1}px`
+          }
+        }>
+          <span className="firstName">{term.firstName}, </span>
+          <span className="lastName">{term.lastName} </span>
+        </div>
+        <div className={`term-info ${term.party}`} key={`info-${index}`} style={
+          {
+            top: `${orderScale*(Number(term.ordningsnummer))}px`,
+            left:`${dayScale * utils.daysBetween("1991-09-30",term.from)}px`,
+          }
+        }>
+          <span className="firstName">{term.firstName}, </span>
+          <span className="lastName">{term.lastName} </span>
+          <span className="lastName">({term.party}) </span>
+        <br />
+          <span className="from">{term.from}</span> - 
+           <span className="to">{term.to}</span>
+           <br />
+          <img src={term.photoUrl} alt={`$term.firstName $term.lastName`}/>_ 
+        </div>
+        </>
+      )
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderTerms()}
     </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,37 @@
+import { data } from './data.js';
+
+
+export const getMembersOfParliament = () => {
+    return data.personlista.person.filter(
+        person => person.personuppdrag.uppdrag.filter(uppdrag => uppdrag.roll_kod === "Riksdagsledamot").length > 0
+        )
+}
+
+export const getTerms = () => {
+    let terms = [];
+    const membersOfParliament = getMembersOfParliament(); 
+    for (const member of membersOfParliament) {
+        for (const uppdrag of member.personuppdrag.uppdrag) {
+            if (uppdrag.roll_kod === "Riksdagsledamot" && uppdrag.ordningsnummer !== "0") {
+                terms.push({
+                    ordningsnummer: uppdrag.ordningsnummer, 
+                    from: uppdrag.from, 
+                    to:uppdrag.tom, 
+                    party:member.parti, 
+                    firstName:member.tilltalsnamn, 
+                    lastName:member.efternamn,
+                    photoUrl: member.bild_url_192 
+                });
+            }
+        }
+    }
+    return terms
+
+}
+
+export const daysBetween = (date1, date2) => {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(date1);
+    const secondDate = new Date(date2);
+    return Math.round((secondDate.getTime() - firstDate.getTime()) / (oneDay));
+}
